b15e6c9c8c7a8c887841aac10c3b192cf48ea421
Dzintars <dzintars.klavins@gmail.com>
2021-05-24 20:18:24 +0300

    chore(fmt): little code formatting

--- versions.tf
+++ versions.tf
@@ -1,13 +1,13 @@
 terraform {
   required_version = ">= 0.14.7"
-  experiments = [module_variable_optional_attrs]
+  experiments      = [module_variable_optional_attrs]
   required_providers {
     libvirt = {
       source  = "dmacvicar/libvirt"
       version = "~>0.6.3"
     }
     macaddress = {
-      source = "ivoronin/macaddress"
+      source  = "ivoronin/macaddress"
       version = "~>0.2.2"
     }
   }
